8790e514192e6c5202fc0dde80e974de9f36976e
Imran Charania <67484264+imrancharania@users.noreply.github.com>
2024-12-31 11:29:24 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/minerva-landing-page",
   output: "export",
   images: {
     unoptimized: true,
